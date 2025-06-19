de08cbe929673c57837bfb74309288df58f4c1b8
hasmukhdabhi <skey.hasmukh@gmail.com>
2025-06-19 14:15:12 +0530

    css and js add

---
+++ wp-content/plugins/Salon Booking/assets/js/script.js
@@ -0,0 +1,4 @@
+// This file is currently empty but is enqueued.
+// You can add JavaScript for form validation or an interactive calendar here.
+
+console.log('Salon Booking Plugin script loaded.');
